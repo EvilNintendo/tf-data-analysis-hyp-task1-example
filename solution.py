20a5f71405fc3746164158af11a66700dc4ff223
Kirill <explorerpeople123@gmail.com>
2023-04-13 19:25:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import norm
 
 chat_id = 134277149 # Ваш chat ID, не меняйте название переменной
 
@@ -11,11 +11,11 @@ def solution(x_success: int,
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
     p = (x_success + y_success) / (x_cnt + y_cnt)
-# Вычисляем z-статистику
+    # Вычисляем z-статистику
     z = (p1 - p2) / np.sqrt(p * (1 - p) * (1/x_cnt + 1/y_cnt))
-# Вычисляем критическое значение z
+    # Вычисляем критическое значение z
     z_critical = norm.ppf(1 - 0.06/2)
-# Проверяем, попадает ли z в доверительный интервал
+    # Проверяем, попадает ли z в доверительный интервал
     if abs(z) > z_critical:
         return True
     else:
